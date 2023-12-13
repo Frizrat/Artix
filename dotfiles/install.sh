5e31392938dd9e8519055605af40f942f53d6f10
Frizrat <80044552+Frizrat@users.noreply.github.com>
2023-12-13 20:35:50 +0100

    Update install.sh

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -2,5 +2,6 @@
 
 sudo pacman --needed -S rxvt-unicode rofi polybar i3 picom
 
+mkdir ~/.config/gtk-3.0 ~/.config/i3 ~/.config/i3status ~/.config/picom ~/.config/polybar ~/.config/rofi ~/.config/urxvt
 cp Xresources ~/.Xresources
 cp -R config ~/.config
